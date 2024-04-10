69e3a13dca89387e62b5a72a0b2fef6d24208852
mazerunner321 <prajwalhg321@gmail.com>
2024-04-10 22:50:55 +0530

    Img on hover

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+let elemC = document.querySelector("#elem_container");
+let fixedImg = document.getElementById("fixed_image");
+
+elemC.addEventListener("mouseenter", function () {
+  fixedImg.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixedImg.style.display = "none";
+});
